8ef669208faabbe8d06a33a57d621fc176032175
Divyani Rai <divyanir6@gmail.com>
2025-07-03 17:16:48 +0530

    updated

--- src/main/java/com/demo/calculatorMain.java
+++ src/main/java/com/demo/calculatorMain.java
@@ -5,9 +5,9 @@ public class calculatorMain {
 	public static void main(String[] args) {
 		calculator cal = new calculator();
 		int sum = cal.add(10, 20);
-		System.out.println(sum);
+		System.out.println("total is :" +sum);
 		int sub = cal.substract(30, 5);
-		System.out.println(sub);
+		System.out.println("result is :" +sub);
 
 	}
 
